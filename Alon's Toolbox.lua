57d568bad82730ec6b540fba9a390a671d8308ab
alon <72901838+Aloncheeto@users.noreply.github.com>
2023-09-03 21:02:25 -0400

    order a little bit better + added warning

--- Alon's Toolbox.lua
+++ Alon's Toolbox.lua
@@ -1,3 +1,5 @@
+--IF YOU ARE GOING TO SKID YOU BETTER KNOW THAT MY CODE IS PROBABLY WORSE THAN YOURS, PLEASE TRY TO DO IT YOURSELF.
+--AlonsToolbox 0.02
 util.require_natives("natives-1681379138", "g-uno")
 local interface = menu.list(menu.my_root(), "UI Tweaks")
 local experiment = menu.list(menu.my_root(), "Experiments")
@@ -5,11 +7,8 @@ local credits = menu.list(menu.my_root(), "Credits")
 util.create_tick_handler(function()
     music = GET_AUDIBLE_MUSIC_TRACK_TEXT_ID()
 end)
-interface:slider("Online Menu Colours", {"In hue, obviously"}, "In hue, obviously", 0, 360, 0, 1, function(amount)
-    MENUHUE = amount
-end)
 --EXPERIMENT START--
-experiment:action("Cat Create", {}, "Testing ", function() entities.create_ped(1, util.joaat("a_c_cat_01"), players.get_position(players.user() ), 0)  end) --n
+experiment:action("Cat Create", {}, "Testing ", function() util.requestmodel() entities.create_ped(1, util.joaat("a_c_cat_01"), players.get_position(players.user() ), 0)  end) --n
 experiment:action("MusicTest", {}, "prints current music label  ", function()  end) 
 experiment:action("Current Vehicle Name", {}, "test", function() ptr = entities.get_user_vehicle_as_pointer(true) vehcur = entities.get_model_hash(ptr) finalstring = util.reverse_joaat(vehcur)  util.toast(finalstring) end) 
 --CREDITS START--
